6ba6c566820927e452d1ba86225a2280f18afd10
kayteawest <kaytea.westcott@gmail.com>
2023-01-26 18:13:11 -0700

    Update

---
+++ src/com/dmtool/models/Encounters.java
@@ -0,0 +1,11 @@
+package com.dmtool.models;
+
+import java.time.LocalDateTime;
+
+public class Encounters {
+	
+	  int encounterId; // Encounter ID
+	  int campaignId; // ID of the Campaign the Encounter belongs to
+	  LocalDateTime createdDate; // LocalDateTime the Encounter was created
+
+}
